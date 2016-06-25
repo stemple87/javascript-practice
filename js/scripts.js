badb63e9aaaa0d5669a251d9a7d09aead3acf7a1
simon <simonctemple@live.com>
2016-06-24 21:10:37 -0700

    broken version

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,29 @@
-//.sort() and the method I wrote here are sorting the array globally, confused as to why.
-var findSmallest = function(sortedArray) {
-  var counter = 0;
-  for (var i = 0; i < sortedArray.length; i++) {
-    counter ++;
-    if(sortedArray[i] > sortedArray[i + 1]) {
-      var largerNum = sortedArray[i]
-      sortedArray.splice(i, 1)
-      sortedArray.push(largerNum);
-      i -= counter;
-      counter = 0;
-    }
-  }
-  return sortedArray[0];
-}
-
-var removeSmallest = function(numbers, lowNumber) {
-  for (var j = 0; j < numbers.length; j++)
-  {
-    if (numbers[j] === lowNumber)
-    {
-      numbers.splice(j, 1);
-      break;
-    }
-  }
-  return numbers;
+function removeSmallest(numbers) {
+  debugger;
+  var lowNumber = 0;
+  const myNumbers = numbers;
+  var Module = (function () {
+    return {
+      getSmallestNumber: function () {
+        numbers.sort();
+        var s = numbers[0];
+        return s;
+      },
+      removeSmallestNumber: function () {
+        for (var j = 0; j < numbers.length; j++){
+          if (numbers[j] === lowNumber){
+            numbers.splice(j, 1);
+            break;
+          }
+        }
+        return numbers;
+      }
+    };
+  })();
+  lowNumber = Module.getSmallestNumber();
+  numbers = myNumbers;
+  result = Module.removeSmallestNumber();
+  return result;
 }
 
 $(document).ready(function(){
@@ -35,15 +35,9 @@ $(document).ready(function(){
     for (var i = 0; i < myArray.length; i++) { //this converts the array of strings to intergers
       myArray[i] = +myArray[i];
     }
-
     var param1 = myArray;
     var param2 = myArray;
-
-    console.log("myArray before methods: " + myArray)
-    var smallestNum = findSmallest(param1);
-    console.log("myArray after smallestNum Method: " + myArray)
-    var result = removeSmallest(param2, smallestNum);
-    console.log("myArray after removeSmallest Method: " + myArray)
+    var result = removeSmallest(param1, param2);
     $('#result').append(result);
   });
 });
